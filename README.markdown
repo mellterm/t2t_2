6ed6d3dd11b866d685cd5ec89203af318228e0fd
Vince The Mac Vincent <mellterm@gmail.com>
2011-11-12 00:42:07 -0800

    New README

---
+++ README.markdown
@@ -0,0 +1,17 @@
+# t2t CS186 Project
+
+##A Big Picture View
+
+* Set up the entities Users, Translations, Containers and Auxiliary Tables
+* Actions: CRUD 
+
+##Some TODOS
+
+*   create a user login system/authentification
+* 	concurrent access
+
+
+##Requirements
+
+Please go here for an update listing of the [*Requirements, Data Model *](http://cs.mills.edu/~splatt/projectreqs.html/)
+by [Splatt](mailto:splatt@mills.edu).README.markdown
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+T2t::Application.config.secret_token = '198bdad5c8617d0bb5a014b5470002cf88b806318373c057193e9bf83b8857ca6fc56ebf926e16fa99284c348748eb1a6877b8e1991998207e1a7bfe5e167985'
